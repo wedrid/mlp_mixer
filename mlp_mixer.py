fd2e52d11ebc36badab6f566165b8fab5358f94f
Edoardo W. Grappolini <ewgrappolini@gmail.com>
2022-02-21 09:49:13 +0100

    done mixer layer

--- mlp_mixer.py
+++ mlp_mixer.py
@@ -5,18 +5,19 @@ from torch import nn
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class MLP(nn.Module):
-    def __init__(self, params_lin1, params_lin2):
+    def __init__(self, in_dim, hidden_dim, out_dim):
         '''
         initialization
-        :param params_lin1: tuple in the form (in_features, out_features) of linear layer 1
-        :param params_lin2: tuple in the form (in_features, out_features) of linear layer 2
+        :param in_dim: dimension of input of first FC layer
+        :param hidden_dim: dimension of hidden layer (i.e. dim of out of first FC and dim of in of second FC)
+        :param out_dim: dimension of output of second FC layer
         '''
-
+        assert in_dim == out_dim # ? forse no? sì, perchè dal BLOCCO, input e output non devono cambiare, cfr. schema
         super(MLP, self).__init__()
         self.linear_gelu_stack = nn.Sequential(
-            nn.Linear(params_lin1[0], params_lin1[1]),
+            nn.Linear(in_dim, hidden_dim),
             nn.GELU(),
-            nn.Linear(params_lin2[0], params_lin2[1])
+            nn.Linear(hidden_dim, out_dim)
         )
 
         def forward(self, x):
@@ -31,7 +32,7 @@ class MixerLayer(nn.Module):
 
     Note (*) and (**) represent skip connections
     '''
-    def __init__(self, n_patches, n_channels, hyper_dict):
+    def __init__(self, n_patches, n_channels, hidden_mlp1_dim, hidden_mlp2_dim):
         ''' Mixer layer, described in the class comment
         :param hyper_dict: has hyperparameters needed for the network architecture
             :param mlp1_lin1_dims: tuple describing the dimension of the first linear layer in the first MLP component
@@ -41,11 +42,30 @@ class MixerLayer(nn.Module):
         :param n_patches: number of rows of the input matrix i.e. number of patches 
         :param n_channels: number of columns of the input matrix i.e. number of channels
         '''
-        self.layer_norm1 = nn.LayerNorm(n_channels) # TODO: da capire la dimensione del layernorm, forse dim=n_channels?
+        super().__init__()
+        self.layer_norm1 = nn.LayerNorm(n_channels) # TODO: da capire la dimensione del layernorm, forse dim=n_channels? Sì, n_channels sarebbe la dimensione della rappresentazione dell'embedding del patch
         self.layer_norm2 = nn.LayerNorm(n_channels)
-        self.MLP1 = MLP(hyper_dict['mlp1_lin1_dims'], hyper_dict['mlp1_lin2_dims'])
-        self.MLP2 = MLP(hyper_dict['mlp2_lin1_dims'], hyper_dict['mlp2_lin2_dims'])
+
+        # only two mlps because of weight sharing
+        self.MLP1 = MLP(n_patches, hidden_mlp1_dim, n_patches) # teoricamente num patches, e hidden dimension
+        self.MLP2 = MLP(n_channels, hidden_mlp2_dim, n_channels) # 'token mixing' teoricamente hidden dimension e ..
 
 
-    def forward(x):
-        pass
+    def forward(self, x):
+        '''
+        :param x: has size [batch size, n_patches, n_channels]
+        '''
+        # layer norm, transp, mlp1, transp, layer norm, mlp2
+        to_skip = self.layer_norm1(x)
+        to_skip = to_skip.permute(0, 2, 1) # do the transposition, 0 remains there because it's the batch size
+        to_skip = self.MLP1(to_skip) # weight sharing!!
+        to_skip = to_skip.permute(0, 2, 1) # stesso permute di prima (transposition)
+        x = x + to_skip # skip connections
+        to_skip = self.layer_norm2(x)
+        to_skip = self.MLP2(to_skip)
+        return (x + to_skip)
+
+#TODO: in articolo c'è il discorso su 1x1 convolution
+
+class MLP_mixer(nn.Module):
+    
