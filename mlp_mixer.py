b9cd4fca72b093fc91c3de6db3c22a64c93f41c6
Edoardo W. Grappolini <ewgrappolini@gmail.com>
2022-02-24 13:00:48 +0100

    MLP mixer and forward loop, testing

--- mlp_mixer.py
+++ mlp_mixer.py
@@ -69,8 +69,8 @@ class MLP(nn.Module):
             nn.Linear(hidden_dim, out_dim)
         )
 
-        def forward(self, x):
-            return self.linear_gelu_stack(x)
+    def forward(self, x):
+        return self.linear_gelu_stack(x)
 
 class MixerLayer(nn.Module):
     '''
@@ -83,11 +83,6 @@ class MixerLayer(nn.Module):
     '''
     def __init__(self, n_patches, n_channels, hidden_mlp1_dim, hidden_mlp2_dim):
         ''' Mixer layer, described in the class comment
-        :param hyper_dict: has hyperparameters needed for the network architecture
-            :param mlp1_lin1_dims: tuple describing the dimension of the first linear layer in the first MLP component
-            :param mlp1_lin2_dims: tuple describing the dimension of the second linear layer in the first MLP component
-            :param mlp2_lin1_dims: tuple describing the dimension of the first linear layer in the second MLP component
-            :param mlp2_lin2_dims: tuple describing the dimension of the second linear layer in the second MLP component
         :param n_patches: number of rows of the input matrix i.e. number of patches 
         :param n_channels: number of columns of the input matrix i.e. number of channels
         '''
@@ -99,11 +94,11 @@ class MixerLayer(nn.Module):
         self.MLP1 = MLP(n_patches, hidden_mlp1_dim, n_patches) # teoricamente num patches, e hidden dimension
         self.MLP2 = MLP(n_channels, hidden_mlp2_dim, n_channels) # 'token mixing' teoricamente hidden dimension e ..
 
-
     def forward(self, x):
         '''
         :param x: has size [batch size, n_patches, n_channels]
         '''
+        #print("HELLOOOO")
         # layer norm, transp, mlp1, transp, layer norm, mlp2
         to_skip = self.layer_norm1(x)
         to_skip = to_skip.permute(0, 2, 1) # do the transposition, 0 remains there because it's the batch size
@@ -124,7 +119,7 @@ class MLP_mixer(nn.Module):
         # - Global average pooling
         # - FC finale
 
-    def __init__(self, img_h_w = 36, patch_dim = 12, n_channels = 16,  n_classes = 100, num_mixers_layers = 7, hidden_dim_mlp_token = 10, hidden_dim_mlp_channel = 10, fc_head_in_dim = 1000):
+    def __init__(self, img_h_w = 36, patch_dim = 12, n_channels = 16,  n_classes = 100, num_mixers_layers = 7, hidden_dim_mlp_token = 100, hidden_dim_mlp_channel = 100, debug = False):
         # :param patch_dims: dimensions of patch, will allow calculation of number of patches. Square image => square patches i.e. patch_dims = width = height
         # :param n_channels: aka patch latent representation dimention
         # :param hidden_dim_mlp_token: hidden dimension size for first mlp blocks (token mixing)
@@ -134,16 +129,63 @@ class MLP_mixer(nn.Module):
         ''' 
         NOTE: si assumono immagini quadrate, se non sono quadrate, fare preprocessing aggiungendo padding
         '''
+        self.debug = debug
         super().__init__()
         # 1) self.patch_embedding, in comune con ViT 
         # da (1) viene ricavato n_channels perchè è la dimensione del codice latente
-        num_patches = img_h_w // patch_dim # integer division
+        num_patches = (img_h_w // patch_dim) **2 # integer division, squared because image is a square duh
+        if self.debug: 
+            print(f"NUM PATCHES: {num_patches}")
         self.embedder = PatchEmbedding((img_h_w, img_h_w), patch_size=patch_dim, embed_dim=n_channels)
         self.mixerlayers = nn.ModuleList([MixerLayer(num_patches, n_channels, hidden_dim_mlp_token, hidden_dim_mlp_channel) for _ in range(num_mixers_layers)]) #TODO
-        # should we layer norm before this?
-        self.fc_head = nn.Linear(fc_head_in_dim, n_classes)
+        #self.mixerlayers = MixerLayer(num_patches, n_channels, hidden_dim_mlp_token, hidden_dim_mlp_channel)
+        # in implementation reported in the paper, there is also a layernorm, which is not present in the macroscopic scheme
+        self.pre_fc_layernorm = nn.LayerNorm(n_channels)
+        self.fc_head = nn.Linear(n_channels, n_classes)
 
     def forward(self, x):
-        #1 image tensor to patch embeddings tensor
-        embedding = self.embedder(x) # patch embeddings (batch size, n_patches, n_channels)
-        
+        # 1 image tensor to patch embeddings tensor
+        out = self.embedder(x) # patch embeddings (batch size, n_patches, n_channels) check: [100, 9, 16]
+        # TODO: nel paper c'è scritto "per patch fully connected" ma non mi è molto chiaro
+        if self.debug: 
+            print(f"Embedding size: {out.size()}")
+        # 2 pass through all the mixer layers
+        for layer in self.mixerlayers:
+            out = layer(out)
+        #out = self.mixerlayers(out)
+        #print(f"Before pooing size: {out.size()}")
+        # 3 of the mixed layers, do global average pooling, which is simpy an average of the feature vectors
+        out = out.mean(dim=1)
+        if self.debug:
+            print(f"After pooling size: {out.size()}")
+        out = self.pre_fc_layernorm(out)
+        out = self.fc_head(out)
+        if self.debug: 
+            print(f"After fc {out.size()}")
+        return out
+
+if __name__ == "__main__":
+    import torch
+    import torchvision
+    import torchvision.transforms as transforms
+
+    #hyperparameters
+    batch_size = 100
+
+    pad_totensor_transform = transforms.Compose([transforms.Pad(2), transforms.ToTensor()]) # does the padding, images 32x32 become 36x36 (symmetric increase) so that are divisible by three and patches are 12x12
+
+    train_dataset = torchvision.datasets.CIFAR100(root='./cifar100_data', train=True, transform=pad_totensor_transform, download=True)
+
+    test_dataset = torchvision.datasets.CIFAR100(root='./cifar100_data', train=False, transform=pad_totensor_transform)
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+    # check patch embedding
+    item = iter(train_loader).next()[0]
+    print(f"Size of item: {item.size()}")
+
+    embedder = PatchEmbedding(embed_dim=16, patch_size=12)
+    embedding = embedder(item)
+    print(f"Size of embedding: {embedding.size()}")
+    mlpmixer = MLP_mixer()
+    mlpmixer(item) 
