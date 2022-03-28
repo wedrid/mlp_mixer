3b90e190669ddd0d6c7b8fe90e7b62af6ccd68c7
Edoardo W. Grappolini <ewgrappolini@gmail.com>
2022-03-28 11:09:36 +0200

    archive and fixed train.py

--- archive/error_somewhere.py
+++ archive/error_somewhere.py
@@ -2,14 +2,20 @@ from comet_ml import Experiment
 import torch
 import torchvision
 import torchvision.transforms as transforms
-from utils import * 
-import time
+import matplotlib.pyplot as plt
+
 from mlp_mixer import *
-from tqdm.notebook import tqdm
+#from tqdm.notebook import tqdm
+from tqdm import tqdm
+import numpy as np
 from sklearn.metrics import accuracy_score
 import json
+import time
+from utils import * 
 
 train_portion = 0.7
+
+#controllare che len(val) è len(train)
 
 with open('set_hyper_params.json') as json_file:
     params = json.load(json_file)
@@ -30,21 +36,28 @@ batch_size = params['batch_size']
 randAugm_numops = params['rand_augm_numops']
 randAugm_magn = params['rand_augm_magnitude']
 
-#controllare che len(val) è len(train)
+if mixup_alpha < 0: 
+    mixup = False
+else:
+    mixup = True
 
-#hyperparameters
-batch_size = params['batch_size']
 
 #pad_totensor_transform = transforms.Compose([transforms.Pad(2), transforms.ToTensor()]) # does the padding, images 32x32 become 36x36 (symmetric increase) so that are divisible by three and patches are 12x12
-pad_totensor_transform = transforms.Compose([transforms.ToTensor()]) #no pad, no normalization
+#pad_totensor_transform = transforms.Compose([transforms.ToTensor()]) #no pad, no normalization
 
-root = './cifar100_data' #if not in lab
+my_transforms = transforms.Compose([ 
+                            #transforms.Resize((224,224)),
+                            #transforms.RandAugment(num_ops = randAugm_numops,magnitude = randAugm_magn ),
+                            transforms.ToTensor(), #nota importante, ToTensor dev'essere sempre come ultima trasformazione
+                            ])
+
+#root = './imagenet' #if not in lab
 root = '../datasets/cifar100'
 
 
-dataset = torchvision.datasets.CIFAR100(root=root, train=True, transform=pad_totensor_transform, download=True)
+dataset = torchvision.datasets.CIFAR100(root=root, train=True, transform = my_transforms, download=True)
 train_subset, val_subset = torch.utils.data.random_split(dataset, [int(train_portion*len(dataset)), len(dataset) - int(train_portion*len(dataset))], generator=torch.Generator().manual_seed(1))
-test_dataset = torchvision.datasets.CIFAR100(root=root, train=False, transform=pad_totensor_transform)
+test_dataset = torchvision.datasets.CIFAR100(root=root, train=False, transform = transforms.ToTensor())
 
 
 train_loader = torch.utils.data.DataLoader(dataset=train_subset, shuffle=True, batch_size=batch_size)
@@ -60,63 +73,39 @@ print(f"Val subset len: {len(val_subset)}")
 print(f"Val subset len: {len(val_loader)}")
 print(f"Test: {len(val_subset)/batch_size}")
 
+
 print(f"Test subset len: {len(test_dataset)}")
 print(f"Test subset len: {len(test_loader)}")
 print(f"Test: {len(test_dataset)/batch_size}")
 
-examples = iter(train_loader)
-samples, labels = examples.next()
-print(samples.shape, labels.shape)
+####
 
-import matplotlib.pyplot as plt
-
-img_sample = samples[0]
-print(img_sample.shape)
-print(img_sample.shape)
-#plt.imshow(img_sample.permute(1, 2, 0))
-#plt.show()
-
-
-'''
-def get_accuracy(predicted, true_labels):
-    predicted = torch.argmax(predicted.cpu(), dim=1)
-    return accuracy_score(predicted, true_labels.cpu()) #forse questi passaggi a cpu non sono molto efficienti..
-'''
-def generate_folder():
-    import time
-    import os
-    import os.path
-    datetime = time.strftime("%Y%m%d-%H%M%S")
-
-    folder = f"./models/{datetime}/"
-    os.chdir(".")
-    print("current dir is: %s" % (os.getcwd()))
-
-    if os.path.isdir(folder):
-        print("Exists")
-    else:
-        os.mkdir(folder)
-    return folder
 
 experiment = Experiment(
     api_key="xX6qWBFbiOreu0W3IrO14b9nB",
     project_name="mlp-mixer",
     workspace="wedrid",
 )
+if mixup: 
+    experiment.add_tag('mixup')
+
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+examples = iter(train_loader)
+samples, labels = examples.next()
+
+img_sample = samples[0]
 
 image_width_height = img_sample.shape[1]
 
 model = MLP_mixer(img_h_w=image_width_height, patch_dim=patch_dims, n_channels=n_channels, num_mixers_layers=num_layers,
     hidden_dim_mlp_token=mlp_ds_dimension, hidden_dim_mlp_channel=mlp_dc_dimension) #in this case 2 patches 16x16
-optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate, weight_decay=1e-5) #weight decay 0.1?
+optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate, weight_decay=weight_decay) 
 
-num_epochs = 200
 steps_total = len(train_loader)
 
 #ATTENZIONE: CAMBIARE IPERPARAMETRI ***PRIMAAAA*** DEL DICT SUCCESSIVO
-
 
 hyper_params = {
     "dataset": root,
@@ -140,6 +129,7 @@ hyper_params = {
 }
 
 experiment.log_parameters(hyper_params)
+
 model_path = generate_folder()
 with open(model_path+"/params.json", "w") as file:
     json.dump(hyper_params, file, indent=4)
@@ -150,21 +140,20 @@ for epoch in tqdm(range(num_epochs)):
     start = time.time()
     model.train()
     train_accuracy = 0
-    for i, (images, labels) in enumerate(tqdm(train_loader)):
+    #for i, (images, labels) in enumerate(tqdm(train_loader)):
+    for i, (images, labels) in enumerate(train_loader):
         # [100, 3, 36, 36] is what is returned by iterator
         images = images.to(device)
         labels = labels.to(device)
-        
-        # forward pass
+            # forward pass
         predicted = model(images)
         loss = loss_func(predicted, labels)
+            
         train_accuracy += get_accuracy(predicted, labels)
-
         # backwards pass
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
         if False and (i+1) % 100:
             print(f'epoch: {epoch+1} of {num_epochs}, step {i+1} of {steps_total}, loss = {loss.item():.4f}')
     print(f"Loss of epoch {epoch+1}: {loss.item():.4f}")
@@ -172,16 +161,15 @@ for epoch in tqdm(range(num_epochs)):
     #print(f"TRAIN LOADER LENGTH: {len(train_loader)}")
     end = time.time()
     elapsed = end - start
-    experiment.log_metric("train epoch loss", loss.item(), step=epoch)
-    experiment.log_metric("mean train epoch accuracy", train_accuracy, step=epoch)
+    experiment.log_metric("train epoch loss", loss.item(), step = epoch)
+    experiment.log_metric("mean train epoch accuracy", train_accuracy, step = epoch)
     experiment.log_metric("epoch time", elapsed, step = epoch)
-
     # validation
     with torch.no_grad():
         model.eval()
         val_accuracy = 0
         temp = 0
-        for i, (images, labels) in enumerate(tqdm(val_loader)): #numero esempi/batchsize TODO check
+        for i, (images, labels) in enumerate((val_loader)): #numero esempi/batchsize TODO check
             # [100, 3, 36, 36] is what is returned by iterator
             images = images.to(device)
             labels = labels.to(device)
@@ -192,9 +180,9 @@ for epoch in tqdm(range(num_epochs)):
             val_accuracy += get_accuracy(predicted, labels)
         #print(f"Lenght val loader: {len(val_loader)}, counter: {temp}")
         val_accuracy /= len(val_loader) 
-        experiment.log_metric("val epoch loss", loss.item(), step=epoch)
+        experiment.log_metric("val epoch loss", loss.item(), step=epoch) #TODO average loss?
         experiment.log_metric("mean val epoch accuracy", val_accuracy, step=epoch)
     
-    if epoch % 20 == 0:
+    if epoch % 10 == 0:
         torch.save(model.state_dict(), model_path + f"checkpoint_epch_{epoch}.pth")
-torch.save(model.state_dict(), model_path + f"final.pth") 
+torch.save(model.state_dict(), model_path + f"final.pth")
