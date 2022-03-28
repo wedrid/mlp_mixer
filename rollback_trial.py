2dc3ca7f6f2cb751dcdaf645cba4f41bcf0d69e1
Edoardo W. Grappolini <ewgrappolini@gmail.com>
2022-03-28 10:47:09 +0200

    rollbacktrial

---
+++ rollback_trial.py
@@ -0,0 +1,177 @@
+from comet_ml import Experiment
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+train_portion = 0.7
+
+#controllare che len(val) è len(train)
+
+#hyperparameters
+batch_size = 250
+
+#pad_totensor_transform = transforms.Compose([transforms.Pad(2), transforms.ToTensor()]) # does the padding, images 32x32 become 36x36 (symmetric increase) so that are divisible by three and patches are 12x12
+pad_totensor_transform = transforms.Compose([transforms.ToTensor()]) #no pad, no normalization
+
+root = './cifar100_data' #if not in lab
+root = '../datasets/cifar100'
+
+
+dataset = torchvision.datasets.CIFAR10(root=root, train=True, transform=pad_totensor_transform, download=True)
+train_subset, val_subset = torch.utils.data.random_split(dataset, [int(train_portion*len(dataset)), len(dataset) - int(train_portion*len(dataset))], generator=torch.Generator().manual_seed(1))
+test_dataset = torchvision.datasets.CIFAR10(root=root, train=False, transform=pad_totensor_transform)
+
+
+train_loader = torch.utils.data.DataLoader(dataset=train_subset, shuffle=True, batch_size=batch_size)
+val_loader = torch.utils.data.DataLoader(dataset=val_subset, shuffle=False, batch_size=batch_size)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+print(f"BATCH SIZE: {batch_size}")
+print(f"Tran subset len: {len(train_subset)}")
+print(f"Tran loader len: {len(train_loader)}")
+print(f"Test: {len(train_subset)/batch_size}")
+
+print(f"Val subset len: {len(val_subset)}")
+print(f"Val subset len: {len(val_loader)}")
+print(f"Test: {len(val_subset)/batch_size}")
+
+
+print(f"Test subset len: {len(test_dataset)}")
+print(f"Test subset len: {len(test_loader)}")
+print(f"Test: {len(test_dataset)/batch_size}")
+
+examples = iter(train_loader)
+samples, labels = examples.next()
+print(samples.shape, labels.shape)
+
+import matplotlib.pyplot as plt
+
+img_sample = samples[0]
+print(img_sample.shape)
+print(img_sample.shape)
+plt.imshow(img_sample.permute(1, 2, 0))
+plt.show()
+
+from mlp_mixer import *
+from tqdm.notebook import tqdm
+from sklearn.metrics import accuracy_score
+import json
+
+def get_accuracy(predicted, true_labels):
+    predicted = torch.argmax(predicted.cpu(), dim=1)
+    return accuracy_score(predicted, true_labels.cpu()) #forse questi passaggi a cpu non sono molto efficienti..
+
+def generate_folder():
+    import time
+    import os
+    import os.path
+    datetime = time.strftime("%Y%m%d-%H%M%S")
+
+    folder = f"./models/{datetime}/"
+    os.chdir(".")
+    print("current dir is: %s" % (os.getcwd()))
+
+    if os.path.isdir(folder):
+        print("Exists")
+    else:
+        os.mkdir(folder)
+    return folder
+
+experiment = Experiment(
+    api_key="xX6qWBFbiOreu0W3IrO14b9nB",
+    project_name="mlp-mixer",
+    workspace="wedrid",
+)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+image_width_height = img_sample.shape[1]
+patch_dims = 4
+# variable_name = value #paper value
+n_channels = 32 #128 #256 #100 #512 #embed dim
+loss_func = nn.CrossEntropyLoss()
+learning_rate = 0.001
+num_layers = 3 #8
+mlp_dc_dimension = 128 #512 #1024 #2048 # dc è la dimensione del channel mixing (l'ultimo mlp)
+mlp_ds_dimension = 32 #64 #128 #256 # ds è la dimensione del token mixing (il primo)
+
+model = MLP_mixer(img_h_w=image_width_height, patch_dim=patch_dims, n_channels=n_channels, num_mixers_layers=num_layers,
+    hidden_dim_mlp_token=mlp_ds_dimension, hidden_dim_mlp_channel=mlp_dc_dimension) #in this case 2 patches 16x16
+optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate, weight_decay=1e-5) 
+
+num_epochs = 200
+steps_total = len(train_loader)
+
+#ATTENZIONE: CAMBIARE IPERPARAMETRI ***PRIMAAAA*** DEL DICT SUCCESSIVO
+
+hyper_params = {
+    "dataset": root,
+    "comment": 'added weight decay',
+    "train_size": len(train_loader),
+    "validation_size": len(val_loader),
+    "learning_rate": learning_rate,
+    "epochs": num_epochs,
+    "steps": steps_total,
+    "batch_size": batch_size,
+    "image_width_and_height": image_width_height,
+    "patch_width_and_height": patch_dims,
+    "hidden_dim_size (n_channels)": n_channels,
+    "number_of_layers": num_layers,
+    "mlp_dc_dimension": mlp_dc_dimension,
+    "mlp_ds_dimension": mlp_ds_dimension
+}
+
+experiment.log_parameters(hyper_params)
+model_path = generate_folder()
+with open(model_path+"/params.json", "w") as file:
+    json.dump(hyper_params, file, indent=4)
+
+model.to(device)
+# training loop
+for epoch in tqdm(range(num_epochs)):
+    model.train()
+    train_accuracy = 0
+    for i, (images, labels) in enumerate(tqdm(train_loader)):
+        # [100, 3, 36, 36] is what is returned by iterator
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        # forward pass
+        predicted = model(images)
+        loss = loss_func(predicted, labels)
+        train_accuracy += get_accuracy(predicted, labels)
+
+        # backwards pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if False and (i+1) % 100:
+            print(f'epoch: {epoch+1} of {num_epochs}, step {i+1} of {steps_total}, loss = {loss.item():.4f}')
+    print(f"Loss of epoch {epoch+1}: {loss.item():.4f}")
+    train_accuracy /= len(train_loader)
+    #print(f"TRAIN LOADER LENGTH: {len(train_loader)}")
+    experiment.log_metric("train epoch loss", loss.item(), step=epoch)
+    experiment.log_metric("mean train epoch accuracy", train_accuracy, step=epoch)
+    # validation
+    with torch.no_grad():
+        model.eval()
+        val_accuracy = 0
+        temp = 0
+        for i, (images, labels) in enumerate(tqdm(val_loader)): #numero esempi/batchsize TODO check
+            # [100, 3, 36, 36] is what is returned by iterator
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            # forward pass
+            predicted = model(images)
+            loss = loss_func(predicted, labels)
+            val_accuracy += get_accuracy(predicted, labels)
+        #print(f"Lenght val loader: {len(val_loader)}, counter: {temp}")
+        val_accuracy /= len(val_loader) 
+        experiment.log_metric("val epoch loss", loss.item(), step=epoch)
+        experiment.log_metric("mean val epoch accuracy", val_accuracy, step=epoch)
+    
+    if epoch % 20 == 0:
+        torch.save(model.state_dict(), model_path + f"checkpoint_epch_{epoch}.pth")
+torch.save(model.state_dict(), model_path + f"final.pth") 
