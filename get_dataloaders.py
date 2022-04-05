c7faf6b3f6b5ee070f28b2a7470a50a4396e54b5
Edoardo W. Grappolini <ewgrappolini@gmail.com>
2022-04-05 18:31:49 +0200

    files for finetuning

--- get_dataloaders.py
+++ get_dataloaders.py
@@ -30,6 +30,7 @@ def getCIFAR100Loaders(in_params, root='./cifar100_data'):
     print(f"Test subset len: {len(val_loader)}")
     print(f"Test: {len(test_dataset)/in_params['batch_size']}")
     return train_loader, val_loader, len(dataset.classes)
+    
 
 def getImagenetLoaders(in_params, root='../datasets/imagenet'):
     randAugm_numops = in_params['rand_augm_numops']
@@ -57,3 +58,35 @@ def getImagenetLoaders(in_params, root='../datasets/imagenet'):
     print(f"Test subset len: {len(val_loader)}")
     print(f"Test: {len(test_dataset)/in_params['batch_size']}")
     return train_loader, val_loader, len(train_dataset.classes)
+
+def getUpsampledCIFAR100Loaders(in_params, root='./cifar100_data'):
+    randAugm_numops = in_params['rand_augm_numops']
+    randAugm_magn = in_params['rand_augm_magnitude']
+    with_rand_augm_transform = transforms.Compose([
+        transforms.RandAugment(num_ops = randAugm_numops,magnitude = randAugm_magn),
+        transforms.Resize([64,64]),
+        transforms.ToTensor()]) #no pad, no normalization
+    no_rand_augm_transform = transforms.Compose([
+        transforms.Resize([64,64]),
+        transforms.ToTensor()])
+
+    dataset = torchvision.datasets.CIFAR100(root=root, train=True, transform=with_rand_augm_transform, download=True)
+    test_dataset = torchvision.datasets.CIFAR100(root=root, train=False, transform=no_rand_augm_transform)
+
+
+    train_loader = torch.utils.data.DataLoader(dataset=dataset, shuffle=True, batch_size=in_params['batch_size'])
+    val_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=in_params['batch_size'], shuffle=False)
+
+    print(f"BATCH SIZE: {in_params['batch_size']}")
+    print(f"Tran subset len: {len(dataset)}")
+    print(f"Tran loader len: {len(train_loader)}")
+    print(f"Test: {len(dataset)/in_params['batch_size']}")
+
+    print(f"Val/test subset len: {len(test_dataset)}")
+    print(f"Val/test subset len: {len(val_loader)}")
+    print(f"Val/Test: {len(test_dataset)/in_params['batch_size']}")
+
+    print(f"Test subset len: {len(test_dataset)}")
+    print(f"Test subset len: {len(val_loader)}")
+    print(f"Test: {len(test_dataset)/in_params['batch_size']}")
+    return train_loader, val_loader, len(dataset.classes)
